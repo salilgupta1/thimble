8e5d94d3460c20f7b7df461f2f1d7ff18770338a
salilgupta1 <salil.gupta323@gmail.com>
2015-01-20 20:04:03 -0600

    project rename

--- ednu/apps/landingpage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from ednu.apps.landingpage.views import views
-
-urlpatterns = patterns('',
-        url(r'^$', views.landingpage, name='landingpage'),
-)
