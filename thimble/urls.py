73a16d77a0b95fe5895026b994b007dd23f7c968
salilgupta1 <salil.gupta323@gmail.com>
2015-02-03 16:09:54 -0600

    moved url routing around

--- thimble/urls.py
+++ thimble/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^',include('thimble.apps.landingpage.urls', namespace='landingpage')),
+    url(r'^',include('thimble.apps.contests.urls', namespace='contests')),
 )
