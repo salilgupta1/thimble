f4f4c509d7a15b3bccf567f359d9ce78895cea5f
salilgupta1 <salil.gupta323@gmail.com>
2015-02-22 19:08:40 -0600

    moving of templates

--- thimble/apps/Portfolios/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm      
-from cloudinary.forms import CloudinaryJsFileField      
-from thimble.apps.Portfolios.models.schemas.Photo import Photo
-
-class PhotoDirectForm(ModelForm):
-  class Meta:
-      model = Photo
-  image = CloudinaryJsFileField()
