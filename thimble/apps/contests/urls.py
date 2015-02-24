56a3c4ebb6440e77f48752a1088cb7b144db2ac5
salilgupta1 <salil.gupta323@gmail.com>
2015-02-23 19:18:20 -0600

    not needed anymore

--- thimble/apps/contests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from thimble.apps.contests.views import views
-urlpatterns = patterns('',
-	url(r'^$', views.contests, name='contests_page'),
-	url(r'^shirts/$', views.shirts, name='shirts_contest'),
-	url(r'^results/$', views.results, name='results_page'),
-	url(r'^shop/$', views.shop, name='shop_page'),
-)
