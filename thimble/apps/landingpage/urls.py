5c8d7812bd61568f8f23ff7ab78f9b5c0586f4ef
salilgupta1 <salil.gupta323@gmail.com>
2015-04-13 17:06:10 -0500

    reroute home page

--- thimble/apps/landingpage/urls.py
+++ thimble/apps/landingpage/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from thimble.apps.landingpage.views import views
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
-    url(r'old-landingpage/$', views.landingpage, name='old_landingpage')
+    url(r'new-home-page/$', views.home, name='home'),
+    url(r'^$', views.landingpage, name='old_landingpage')
 )
