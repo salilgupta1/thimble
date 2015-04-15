a835692cc7892c63de42be4c94339e8cc89b3e5b
salilgupta1 <salil.gupta323@gmail.com>
2015-04-15 18:24:01 -0500

    new deployment methods

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,15 @@
 from fabric.api import local
 
 def deploy_production():
-	local('heroku maintenance:on')
-	local('git push production master')
-	local('heroku run python manage.py migrate')
-	local('heroku run python manage.py collectstatic')
-	local('heroku maintenance:off')
+	local('heroku maintenance:on --app thimbleapp')
+	local('git push heroku master')
+	local('heroku run python manage.py migrate --app thimbleapp')
+	local('heroku run python manage.py collectstatic --app thimbleapp')
+	local('heroku maintenance:off --app thimbleapp')
 
 def deploy_staging():
-	local('heroku maintenance:on')
+	local('heroku maintenance:on --app staging-thimble')
 	local('git push staging dev')
-	local('heroku run python manage.py migrate')
-	local('heroku run python manage.py collectstatic')
-	local('heroku maintenance:off')
+	local('heroku run python manage.py migrate --app staging-thimble')
+	local('heroku run python manage.py collectstatic --app staging-thimble')
+	local('heroku maintenance:off --app staging-thimble')
