6c792f419befa6a7ef50aae58be73065ecad9813
salilgupta1 <salil.gupta323@gmail.com>
2015-04-21 13:53:14 -0500

    remove collect static

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,10 @@ def deploy_production():
 	local('heroku maintenance:on --app thimbleapp')
 	local('git push heroku master')
 	local('heroku run python manage.py migrate --app thimbleapp')
-	local('heroku run python manage.py collectstatic --app thimbleapp')
 	local('heroku maintenance:off --app thimbleapp')
 
 def deploy_staging():
 	local('heroku maintenance:on --app staging-thimble')
 	local('git push staging dev:master')
 	local('heroku run python manage.py migrate --app staging-thimble')
-	local('heroku run python manage.py collectstatic --app staging-thimble')
 	local('heroku maintenance:off --app staging-thimble')
