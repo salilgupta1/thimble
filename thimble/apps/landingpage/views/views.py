7e7219cc226f13bdf7dda358295a9511c52bf91e
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-04-22 17:41:56 -0500

    Create new homepage with beta invite

--- thimble/apps/landingpage/views/views.py
+++ thimble/apps/landingpage/views/views.py
@@ -43,5 +43,20 @@ def landingpage(request):
 		else:
 			context['success'] = "Thank you for your interest in Thimble!"
 
+	return render(request, "landingpage/new-index.html",context)
+
+def oldlandingpage(request):
+	context = {}
+	if request.method=="POST":
+		email = request.POST.get('email')
+		chimp = chimpy.Connection(os.environ['MAILCHIMP_API_KEY'])
+
+		try:
+			chimp.list_subscribe(os.environ['MAILCHIMP_LIST_ID'], email, {'FIRST': 'unit', 'LAST': 'tests'}, double_optin=False, send_welcome=True)
+		except:
+			context['error'] = "We're sorry, it seems there was an error. This may be because you are already signed up."
+		else:
+			context['success'] = "Thank you for your interest in Thimble!"
+
 	return render(request, "landingpage/old-index.html",context)
 
