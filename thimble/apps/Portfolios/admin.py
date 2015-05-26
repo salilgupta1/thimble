44c9202822c6e12e0445541829e0fbaf4a7e20ea
salilgupta1 <salil.gupta323@gmail.com>
2015-05-26 16:45:26 -0500

    adding pieces to admin cms

--- thimble/apps/Portfolios/admin.py
+++ thimble/apps/Portfolios/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from models.schemas import Comment, Collection, Entry, Like
+from models.schemas import Comment, Collection, Piece, Like
 
 # Register your models here.
 
 admin.site.register(Comment.Comment)
 admin.site.register(Collection.Collection)
-admin.site.register(Entry.Entry)
+admin.site.register(Piece.Piece)
 admin.site.register(Like.Like)
